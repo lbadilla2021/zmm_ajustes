c6ae44941800e292c665d2f26b27760a8cca9c86
lbadilla2021 <lbadilla2021@gmail.com>
2026-04-15 15:11:49 -0400

    Create base Odoo module structure for barca_maintenance

---
+++ barca_maintenance/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Barca Maintenance",
+    "version": "1.0",
+    "summary": "Módulo de mantención de flota Barca SpA",
+    "category": "Operations",
+    "depends": [
+        "fleet",
+        "maintenance",
+        "stock",
+        "purchase",
+        "mail",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/base_views.xml",
+        "data/cron.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
