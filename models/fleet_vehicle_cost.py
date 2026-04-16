980ea2e861d23c7a820bbe2f0db32a86533b9de7
lbadilla2021 <lbadilla2021@gmail.com>
2026-04-16 11:29:43 -0400

    feat: completar capa 1 de mantención barca

--- models/fleet_vehicle_cost.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class FleetVehicleLogServices(models.Model):
-    _inherit = "fleet.vehicle.log.services"
-
-    # Compatibilidad Odoo 18 para vistas/filtros que usan `name` en costos/servicios
-    name = fields.Char(string="Nombre")
